ff772a7fb8703a99c10d7b54a2b1f7f4b0ca96ca
Artem Hordiyevskyy <hordiyevskyy@gmail.com>
2026-07-18 01:15:43 +0300

    Complete final DevOps project

--- Progect/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "terraform-state-bucket-hordiyevskyy77" # Назва S3-бакета
-    key          = "lesson-7/terraform.tfstate"            # Шлях до файлу стейту
-    region       = "eu-central-1"                          # Регіон AWS
-    use_lockfile = true
-    encrypt      = true # Шифрування файлу стейту
-  }
-}
